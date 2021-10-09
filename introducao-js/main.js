280b836802c60165a67d3f2c2c7763e52b49a155
Alexsandro Cristiano <acg.alexsandro@hotmail.com>
2021-10-09 19:11:57 -0300

    adicionar botao que reseta a contagem em introducao-js

--- introducao-js/main.js
+++ introducao-js/main.js
@@ -2,6 +2,12 @@ let count = 0
 
 const CURRENT_NUMBER = document.getElementById('currentNumber')
 
+function zerar() {
+  count = 0
+  activeNumberNegative(count)
+  CURRENT_NUMBER.innerHTML = count
+}
+
 function increment() {
   count++
   activeNumberNegative(count)
