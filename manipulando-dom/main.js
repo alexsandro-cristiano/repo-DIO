91c6904afd2033ce4bd8181c5223fc4c33ca23ad
Alexsandro Cristiano <acg.alexsandro@hotmail.com>
2021-10-10 14:01:38 -0300

    finaliza o projeto dark mode de manipulando DOM

--- manipulando-dom/main.js
+++ manipulando-dom/main.js
@@ -1,23 +1,26 @@
-/**
- * 1 - selecione os elementos h1 button footer e body
- * 2 - se os elementos possuirem a classe dark-mode,
- * modifique seus estilos
- * caso contrario volte os estilos para o original
- *
- *
- */
-
 const h1 = document.getElementById('page-title')
 const botao = document.getElementById('mode-selector')
 const body = document.getElementsByTagName('body')[0]
 const foote = document.getElementsByTagName('footer')[0]
-
+const darkModeClass = 'dark-mode'
 
 function dark() {
-  h1.classList.toggle('dark-mode')
-  botao.classList.toggle('dark-mode')
-  body.classList.toggle('dark-mode')
-  foote.classList.add('dark-mode')
+  h1.classList.toggle(darkModeClass)
+  botao.classList.toggle(darkModeClass)
+  body.classList.toggle(darkModeClass)
+  foote.classList.add(darkModeClass)
+  mudarTexto()
+}
+
+function mudarTexto() {
+  if (botao.classList.contains(darkModeClass)) {
+    botao.innerHTML = 'Light Mode';
+    h1.innerHTML = "Dark Mode ON";
+  }
+  else {
+    botao.innerHTML = 'Dark Mode';
+    h1.innerHTML = "Light Mode ON";
+  }
 }
 
 botao.addEventListener('click', dark)
